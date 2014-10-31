c97951f49b86b78ee084af143d4ea4f6b7151cc2
reniat <reniat314@gmail.com>
2014-10-30 22:58:06 -0500

    Initial repository setup

---
+++ src/org/uiowa/cs2820/engine/Storage.java
@@ -0,0 +1,22 @@
+package org.uiowa.cs2820.engine;
+import java.util.*;
+
+final class Storage{
+	
+	
+	public Storage(){
+		
+	}
+	
+	public static void write(ArrayList<Field> FieldList){
+		
+	}
+	
+	public static Field 
+	
+	private Field read(byte[] bytearray){
+		
+	}
+	
+	
+}
